6aebf2d3c00c094824fa291c8ed9a7eb6acf5da1
Tom Youngbloods <tomjongbloets@live.nl>
2020-04-20 15:27:06 +0200

    Push PWA

---
+++ dist/precache-manifest.242885f7e0fee26f3f94038331428bf0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "095131d2fac428a5b1a5",
+    "url": "/css/app.98253138.css"
+  },
+  {
+    "revision": "dde978b1962dd52d8f2b",
+    "url": "/css/chunk-vendors.5b76c1c9.css"
+  },
+  {
+    "revision": "63bf9de3e1300e6a10a04c654205960d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "095131d2fac428a5b1a5",
+    "url": "/js/app.c397b41d.js"
+  },
+  {
+    "revision": "dde978b1962dd52d8f2b",
+    "url": "/js/chunk-vendors.205358de.js"
+  },
+  {
+    "revision": "0148e72282e0eb3f346505c71cbc6164",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import router from './router'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import './registerServiceWorker'
 
 
 
