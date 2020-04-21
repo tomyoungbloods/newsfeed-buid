31ff71340e50f6d91a9daa4322e1a122048e5ae7
Tom Youngbloods <tomjongbloets@live.nl>
2020-04-21 11:26:09 +0200

    update

--- vue.config.js
+++ vue.config.js
@@ -1,37 +1,25 @@
 module.exports = {
-  // pwa: {
-  //   workboxPluginMode: 'GenerateSW',
-  //   workboxOptions: {
-  //     navigateFallback: '/index.html',
-  //     runtimeCaching: [
-  //       {
-  //         urlPattern: new RegExp('^https://api.zippopotam.us/us/'),
-  //         handler: 'networkFirst',
-  //         options: {
-  //           networkTimeoutSeconds: 20,
-  //           cacheName: 'api cache',
-  //           cacheableResponse: {
-  //             statuses: [0, 200]
-  //           }
-  //         }
-  //       }
-  //     ]
-  //   }
-  // },
   pwa: {
-    name: 'My App',
     themeColor: '#4DBA87',
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black-translucent',
-
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
+    workboxPluginMode: 'GenerateSW',
     workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js',
-      // ...other Workbox options...
       navigateFallback: '/index.html',
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('^https://api.zippopotam.us/us/'),
+          handler: 'networkFirst',
+          options: {
+            networkTimeoutSeconds: 20,
+            cacheName: 'api cache',
+            cacheableResponse: {
+              statuses: [0, 200]
+            }
+          }
+        }
+      ]
     }
   },
   "transpileDependencies": [
