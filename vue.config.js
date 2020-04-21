8248d6416d0c8ac39774cb2463e1e0e99e7c1487
Tom Youngbloods <tomjongbloets@live.nl>
2020-04-21 11:20:12 +0200

    System bar

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,37 @@
 module.exports = {
+  // pwa: {
+  //   workboxPluginMode: 'GenerateSW',
+  //   workboxOptions: {
+  //     navigateFallback: '/index.html',
+  //     runtimeCaching: [
+  //       {
+  //         urlPattern: new RegExp('^https://api.zippopotam.us/us/'),
+  //         handler: 'networkFirst',
+  //         options: {
+  //           networkTimeoutSeconds: 20,
+  //           cacheName: 'api cache',
+  //           cacheableResponse: {
+  //             statuses: [0, 200]
+  //           }
+  //         }
+  //       }
+  //     ]
+  //   }
+  // },
   pwa: {
-    workboxPluginMode: 'GenerateSW',
+    name: 'My App',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black-translucent',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
     workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'dev/sw.js',
+      // ...other Workbox options...
       navigateFallback: '/index.html',
-      runtimeCaching: [
-        {
-          urlPattern: new RegExp('^https://api.zippopotam.us/us/'),
-          handler: 'networkFirst',
-          options: {
-            networkTimeoutSeconds: 20,
-            cacheName: 'api cache',
-            cacheableResponse: {
-              statuses: [0, 200]
-            }
-          }
-        }
-      ]
     }
   },
   "transpileDependencies": [
