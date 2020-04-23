6f5ea45a67251f5bf294189e6288d7daaf62ee22
Tom Youngbloods <tomjongbloets@live.nl>
2020-04-23 14:13:32 +0200

    no message

--- src/components/MethodeTwee.vue
+++ src/components/MethodeTwee.vue
@@ -3,7 +3,7 @@
         <v-row class="no-padding">
             <v-col class="no-padding">    
                         <v-img
-                        src="https://firebasestorage.googleapis.com/v0/b/nieuwslandschap.appspot.com/o/news%2FScreenshot%202020-04-22%20at%2022.22.19.png?alt=media&token=fa7e28ee-b438-45ea-ba68-67acbf51bcd5"
+                        src="https://firebasestorage.googleapis.com/v0/b/nieuwslandschap.appspot.com/o/news%2FPHOTO-2020-04-17-14-10-56.jpg?alt=media&token=fe00e1c5-c687-4a86-b53f-0aa33b0ebf42"
                         class="white--text align-end border-radius-full"
                         gradient="to bottom, rgba(255,255,255,0) 40%, rgba(80,181,242,1)"
                         height="40vh"
