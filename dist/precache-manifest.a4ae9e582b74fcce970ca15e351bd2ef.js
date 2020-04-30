6f42cb7c82f551b1c565c6eeb263b4d62e5d5531
Tom Youngbloods <tomjongbloets@live.nl>
2020-04-30 15:36:53 +0200

    new build

---
+++ dist/precache-manifest.a4ae9e582b74fcce970ca15e351bd2ef.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "581f8092790ed6b81457",
+    "url": "/css/app.d1cf9048.css"
+  },
+  {
+    "revision": "9d3267c9bf48ffe157e9",
+    "url": "/css/chunk-vendors.96d65c7b.css"
+  },
+  {
+    "revision": "beaf971aa8c1a3f564a4a250fe5deac9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "581f8092790ed6b81457",
+    "url": "/js/app.36be72c2.js"
+  },
+  {
+    "revision": "9d3267c9bf48ffe157e9",
+    "url": "/js/chunk-vendors.e564dfef.js"
+  },
+  {
+    "revision": "0148e72282e0eb3f346505c71cbc6164",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "067bca9e716827d4dc9918517c337805",
+    "url": "/splashscreens/ipad_splash.png"
+  },
+  {
+    "revision": "420607a17e6e9a10a865a0ade9394841",
+    "url": "/splashscreens/ipadpro1_splash.png"
+  },
+  {
+    "revision": "f2c74e54af400ea57f20938aa09fb1a3",
+    "url": "/splashscreens/ipadpro2_splash.png"
+  },
+  {
+    "revision": "7642fbab21ab6476b0eb12acd6211f80",
+    "url": "/splashscreens/ipadpro3_splash.png"
+  },
+  {
+    "revision": "e69ab9a5f77813e4e842762c61542866",
+    "url": "/splashscreens/iphone5_splash.png"
+  },
+  {
+    "revision": "1cfd752493e6ab8822cdfad75b453c71",
+    "url": "/splashscreens/iphone6_splash.png"
+  },
+  {
+    "revision": "6bf62b68deef02afd13b140910c084be",
+    "url": "/splashscreens/iphoneplus_splash.png"
+  },
+  {
+    "revision": "0f5990079474337774b387fdc53ade23",
+    "url": "/splashscreens/iphonex_splash.png"
+  },
+  {
+    "revision": "2792f4a049e8b87b50f2d3a96c0bba4c",
+    "url": "/splashscreens/iphonexr_splash.png"
+  },
+  {
+    "revision": "13513675e99d2f7723002c1e7a1bc27b",
+    "url": "/splashscreens/iphonexsmax_splash.png"
+  }
+]);
