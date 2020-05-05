6c1013551d1839fbe95add53ee87ddd5add649d4
Tom Youngbloods <tomjongbloets@live.nl>
2020-05-05 20:27:07 +0200

    twitter install

---
+++ src/components/twitter/MethodeVijf.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="methode-vijf">
+        Hello
+    </div>
+</template>
+<script>
+var Twit = require('twit')
+
+// var T = new Twit({
+//   consumer_key:         '...',
+//   consumer_secret:      '...',
+//   access_token:         '...',
+//   access_token_secret:  '...',
+//   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+//   strictSSL:            true,     // optional - requires SSL certificates to be valid.
+// })
+
+T.get('search/tweets', { q: 'banana since:2011-07-11', count: 2 }, function(err, data, response) {
+  console.log(data)
+});
+
+
+</script>
+
+
