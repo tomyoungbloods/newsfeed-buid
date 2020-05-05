74a678ce1c125e0cd731bea51b8b1f6d0f52035e
Tom Youngbloods <tomjongbloets@live.nl>
2020-05-05 19:49:00 +0200

    color update primary

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,16 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+          light: {
+            primary: '#50b5f2',
+            secondary: '#b0bec5',
+            accent: '#8c9eff',
+            error: '#b71c1c',
+          },
+        },
+      },
 });
+
+ 
