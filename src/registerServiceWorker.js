f405c4c73a8358e5cac351914d84350e3ed1f63c
Tom Youngbloods <tomjongbloets@live.nl>
2020-05-06 14:25:30 +0200

    Alert test

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -20,6 +20,7 @@ if (process.env.NODE_ENV === 'production') {
       console.log('New content is downloading.')
     },
     updated () {
+      alert('New content is available; please refresh.')
       console.log('New content is available; please refresh.')
     },
     offline () {
