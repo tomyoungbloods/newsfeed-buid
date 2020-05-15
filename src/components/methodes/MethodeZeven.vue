843e92410558d1e7ba4108d192fa5b9aeef1b79d
Tom Youngbloods <tomjongbloets@live.nl>
2020-05-15 17:01:59 +0200

    test tweets

--- src/components/methodes/MethodeZeven.vue
+++ src/components/methodes/MethodeZeven.vue
@@ -9,14 +9,14 @@ var Twit = require('twit')
 var T = new Twit({
   consumer_key:         'p9dYauvFpVsXhC4KeWKk7Vrc8',
   consumer_secret:      '039L9wKEeTNxlsoFoXKruwVEgmtaGz1xQPVyDlMGQyNnVAWl1T',
-  access_token:         'p9dYauvFpVsXhC4KeWKk7Vrc8',
-  access_token_secret:  '039L9wKEeTNxlsoFoXKruwVEgmtaGz1xQPVyDlMGQyNnVAWl1T',
+  access_token:         '239511634-5W8L1ho1rgFr1iJapIsaYURJ5hjPD0txVm4doRfJ',
+  access_token_secret:  'v0lj76LnGZOK9CZZPDe8B3oCYSoo1CPi7peXfWJBHuSD7',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
   strictSSL:            true,     // optional - requires SSL certificates to be valid.
 });
 
 
-T.get('search/tweets', { q: 'banana since:2011-07-11', count: 100 }, function(err, data, response) {
+T.get('search/tweets', { q: 'banana since:2018-07-11', count: 2 }, function(err, data, response) {
   console.log(data)
 });
 
